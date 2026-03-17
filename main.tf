ad09934f3db3263d27363ebcbd263e51a3aea45f
alijutt313 <aliahtsham25@proton.me>
2026-03-17 22:11:43 +0500

    Project 1 Attempt 1

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 # 1. Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "DevOps-Final-Fresh-Start"
+  name     = "Project_1"
   location = "France Central" 
 }
 
@@ -15,77 +15,28 @@ resource "random_string" "suffix" {
   upper   = false
 }
 
-# 3. Container Registry
-resource "azurerm_container_registry" "acr" {
-  name                = "registryali${random_string.suffix.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
-  admin_enabled       = true
+resource "azurerm_storage_account" "asa" {
+  name                     = "project1storageacc${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
 }
 
-# 4. Log Analytics Workspace (The Database for logs)
-resource "azurerm_log_analytics_workspace" "logs" {
-  name                = "ali-devops-workspace"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
+resource "azurerm_storage_container" "asc {
+  name                  = "project1storagecontainer${random_string.suffix.result}"
+  storage_account_id    = azurerm_storage_account.asa.id
+  container_access_type = "private"
 }
 
-# 5. Application Insights (The Monitoring Dashboard)
-resource "azurerm_application_insights" "aai" {
-  name                = "ali-app-insights"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  workspace_id        = azurerm_log_analytics_workspace.logs.id
-  application_type    = "web"
+resource "azurerm_storage_blob" "asb" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.asa.name
+  storage_container_name = azurerm_storage_container.asc.name
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "index.html"
 }
 
-# 6. Service Plan (Free Tier)
-resource "azurerm_service_plan" "plan" {
-  name                = "ali-free-plan"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Linux"
-  sku_name            = "F1"
-}
 
-# 7. Linux Web App with Insights Connection
-resource "azurerm_linux_web_app" "webapp" {
-  name                = "ali-devops-app-final"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  service_plan_id     = azurerm_service_plan.plan.id
-
-  # Connects the App to the Monitoring Dashboard
-  app_settings = {
-    "APPLICATIONINSIGHTS_CONNECTION_STRING"      = azurerm_application_insights.aai.connection_string
-    "ApplicationInsightsAgent_EXTENSION_VERSION" = "~3"
-  }
-
-  site_config {
-    always_on = false
-    application_stack {
-      docker_image_name        = "nginx:latest"
-      docker_registry_url      = "https://${azurerm_container_registry.acr.login_server}"
-      docker_registry_username = azurerm_container_registry.acr.admin_username
-      docker_registry_password = azurerm_container_registry.acr.admin_password
-    }
-  }
-}
-
-# --- OUTPUTS ---
-
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "webapp_name" {
-  value = azurerm_linux_web_app.webapp.name
-}
-
-output "app_insights_instrumentation_key" {
-  value     = azurerm_application_insights.aai.instrumentation_key
-  sensitive = true
-}
